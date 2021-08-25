cb2316c66d8933813bba3e2ce7ca0abc87fd4798
Shahed Ahmad <shaheda.a16@gmail.com>
2021-08-25 21:29:35 +0430

    model for a tweet

--- twitter/tweet/models.py
+++ twitter/tweet/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class Tweet(models.Model):
+    user = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    text = models.CharField(max_length=160)
+    created_at = models.DateTimeField(auto_now_add=True)
