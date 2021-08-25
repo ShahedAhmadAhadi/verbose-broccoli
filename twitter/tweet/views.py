3e51ec411139b885aa673fd84bfa1735e57d578d
Shahed Ahmad <shaheda.a16@gmail.com>
2021-08-25 21:34:36 +0430

    new view for tweet

--- twitter/tweet/views.py
+++ twitter/tweet/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.Response import Response
 
 # Create your views here.
+
+def tweet(request):
+    if request.method == 'GET':
+        pass
+    elif request.method == "POST":
+        pass
