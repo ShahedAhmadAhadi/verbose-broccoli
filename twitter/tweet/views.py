0e522c21c1e7db750a1fc6a0f51f2c7f7efd96dd
Shahed Ahmad <shaheda.a16@gmail.com>
2021-08-25 21:54:48 +0430

    url made and view for tweet can make get and post request in test

--- twitter/tweet/views.py
+++ twitter/tweet/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.Response import Response
+from .models import Tweet
+from .serializers import TweetSeializer
+from rest_framework.parsers import JSONParser
 
 # Create your views here.
 
 def tweet(request):
     if request.method == 'GET':
-        pass
+        tweet = Tweet.objects.all()
+        tweet_serializer = TweetSeializer(tweet, many=True)
+        return Response(tweet_serializer.data)
     elif request.method == "POST":
-        pass
+        data = JSONParser().parse(request)
+        tweet_serializer = TweetSeializer(data=data)
+        if tweet_serializer.is_valid():
+            tweet_serializer.save()
+            return Response(tweet_serializer.data)
+        return Response(tweet_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- twitter/twitter/urls.py
+++ twitter/twitter/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tweet import views as tweet_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', tweet_view.tweet),
 ]
