0ad0b29e98249c176da8a7b3cb2c47e11f8c503d
Shahed Ahmad <shaheda.a16@gmail.com>
2021-08-26 22:59:46 +0430

    urls added for djangorestframework-simplejwt and accessed and tested

--- twitter/twitter/urls.py
+++ twitter/twitter/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from tweet import views as tweet_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', tweet_view.tweet),
+    path('accounts/', include('accounts.urls')),
 ]
