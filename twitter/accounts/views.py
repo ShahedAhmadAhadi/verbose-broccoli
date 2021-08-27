ea1b02790cf80a322e02678732308835b8eb2877
Shahed Ahmad <shaheda.a16@gmail.com>
2021-08-27 23:02:47 +0430

    view make for registry phase 1

--- twitter/accounts/views.py
+++ twitter/accounts/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from rest_framework import serializers, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserElementryDataSerializer
 
 # Create your views here.
+@api_view(['POST'])
+def register_phase_1(request):
+    data = request.data
+    serializer = UserElementryDataSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
 
-def register(request):
-    pass
+
+    user_data = serializer.data
+    return Response(user_data, status=status.HTTP_201_CREATED)
+
