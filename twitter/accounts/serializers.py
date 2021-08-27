964fbbf4df3e3d128040647fbee56ff533d40639
Shahed Ahmad <shaheda.a16@gmail.com>
2021-08-27 22:34:00 +0430

    new model created for user data and added to admin site

--- twitter/accounts/serializers.py
+++ twitter/accounts/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import UserElementryData
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=64, min_length=8, write_only=True)
 
     class Meta:
-        model = User
+        model = UserElementryData
         fields = ['first_name', 'last_name', 'email']
