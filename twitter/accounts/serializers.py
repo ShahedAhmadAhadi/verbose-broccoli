89b06d7623375a30e950600c7993aec81ffce5c0
Shahed Ahmad <shaheda.a16@gmail.com>
2021-08-28 21:36:24 +0430

    new factors added for more control in saving data into models

--- twitter/accounts/serializers.py
+++ twitter/accounts/serializers.py
@@ -1,26 +1,31 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import UserElementryData
+from rest_framework.parsers import JSONParser
 
 
 def response_maker_validator(*args, **kwargs):
     response_dict = {}
     for i in kwargs:
-        if i == 'username' or i == 'email':
-            if kwargs[i] == None or kwargs[i] == '':
-                response_dict[i] = '[This field is required]'
-        elif i == 'username':
+        if i == "username" or i == "email":
+            if kwargs[i] == None or kwargs[i] == "":
+                response_dict[i] = "[This field is required]"
+        elif i == "username":
             if not kwargs[i].isalnum():
-                response_dict[i] = '[{response_dict[i]} can only be [0-9], [a-z], [A-Z]]'
+                response_dict[
+                    i
+                ] = "[{response_dict[i]} can only be [0-9], [a-z], [A-Z]]"
         else:
-            if kwargs[i] == None or kwargs[i] == '':
-                response_dict[i] = '[This field is required]'
+            if kwargs[i] == None or kwargs[i] == "":
+                response_dict[i] = "[This field is required]"
             elif not kwargs[i].isalpha():
-                response_dict[i] = '[username can only be [a-z], [A-Z]]'
+                response_dict[i] = "[username can only be [a-z], [A-Z]]"
 
     return response_dict
-        
 
+
+def duplicate_email():
+    pass
 
 class UserElementryDataSerializer(serializers.ModelSerializer):
 
@@ -29,23 +34,28 @@ class UserElementryDataSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email']
+        fields = ["first_name", "last_name", "email"]
 
     def validate(self, attrs):
-        first_name = attrs.get('first_name', '')
-        last_name = attrs.get('last_name', '')
-        email = attrs.get('email', '')
-        print(first_name)
-        validation_response =  response_maker_validator(first_name=first_name, last_name=last_name, email=email)
-        
-        return validation_response
+        first_name = attrs.get("first_name", "")
+        last_name = attrs.get("last_name", "")
+        email = attrs.get("email", "")
+        if UserElementryData.objects.filter(email=email):
+            pass
+
+        validation_response = response_maker_validator(
+            first_name=first_name, last_name=last_name, email=email
+        )
+
+        return validation_response or attrs
 
     def create(self, validated_data):
         return UserElementryData.objects.create(**validated_data)
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(max_length=64, min_length=8, write_only=True)
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email']
+        fields = ["first_name", "last_name", "email"]
--- twitter/accounts/views.py
+++ twitter/accounts/views.py
@@ -3,6 +3,7 @@ from rest_framework import serializers, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import UserElementryDataSerializer
+from .models import UserElementryData
 
 # Create your views here.
 @api_view(["POST"])
