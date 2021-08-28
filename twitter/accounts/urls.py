78aa54913f32a4850503e9dfd6d25cde0daf8d99
Shahed Ahmad <shaheda.a16@gmail.com>
2021-08-29 01:29:43 +0430

    url for sending email and file for sending emails created

--- twitter/accounts/urls.py
+++ twitter/accounts/urls.py
@@ -2,11 +2,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import register_phase_one
+from .views import register_phase_one, verify_email
 from django.urls import path
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("registerE/", register_phase_one),
+    path('email-verify', verify_email, name='verify_email')
 ]
--- twitter/accounts/views.py
+++ twitter/accounts/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from .serializers import UserElementryDataSerializer
 from .models import UserElementryData
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
 
 # Create your views here.
 @api_view(["POST"])
@@ -16,8 +18,20 @@ def register_phase_one(request):
 
     user_data = serializer.data
 
-    email = UserElementryData.objects.get(email=user_data['email'])
 
-    token = RefreshToken.for_user(email)
+    email = UserElementryData.objects.get(email=user_data['email'])
+    token = RefreshToken.for_user(email).access_token
+
+
+    current_site = get_current_site(request).domain
+    relative_link = reverse('verify_email')
+    data = {'domain': current_site.domain}
+
+    absolute_url = 'http://'
+
 
     return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+def verify_email(request):
+    pass
