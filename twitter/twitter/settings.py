97fac1bbcd385faa84cbd3440b21fc44787429a6
Shahed Ahmad <shaheda.a16@gmail.com>
2021-08-28 23:40:53 +0430

    settings for email sending

--- twitter/twitter/settings.py
+++ twitter/twitter/settings.py
@@ -148,3 +148,6 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
