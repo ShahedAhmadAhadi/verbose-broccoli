fd2af9e71f2dbfd8a357a21b6258633e6183a83d
Shahed Ahmad <shaheda.a16@gmail.com>
2021-08-29 23:56:59 +0430

    email sending succeeded and refresh token and subject forward to email correctly the eviroment variables are not processed

--- twitter/twitter/settings.py
+++ twitter/twitter/settings.py
@@ -155,6 +155,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-
+# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+print(os.getenv('EMAIL_HOST_USER'), 'here')
