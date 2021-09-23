23bfce77e34a576c5c405e7e34190f799df4c7ca
Shahed Ahmad <shaheda.a16@gmail.com>
2021-09-23 22:36:45 +0430

    media url and media root added for images of users and serializer for userInfo model created

--- twitter/twitter/settings.py
+++ twitter/twitter/settings.py
@@ -160,6 +160,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
