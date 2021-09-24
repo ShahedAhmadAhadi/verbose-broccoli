199532ff52b8f9b7171ba5dc2c0d325847894456
Shahed Ahmad <shaheda.a16@gmail.com>
2021-09-24 23:04:21 +0430

    error with json parser and error with create method implemented in serializer

--- twitter/user_info/serializers.py
+++ twitter/user_info/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .models import UserInfo
 
 class UserInfoSerializer(serializers.Serializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True)
@@ -7,4 +8,5 @@ class UserInfoSerializer(serializers.Serializer):
     about = serializers.CharField(max_length=255)
     image = serializers.ImageField()
 
-    
+    def create(self, validated_data):
+        return UserInfo.objects.create(**validated_data)
