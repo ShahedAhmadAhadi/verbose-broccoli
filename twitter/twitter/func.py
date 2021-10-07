c843ef28ee88e2dcf0c45b8e747bbe133490ae11
Shahed Ahmad <shaheda.a16@gmail.com>
2021-10-07 20:08:00 +0430

    if token is not valid or refresh token is removed from db exceptions handled

--- twitter/twitter/func.py
+++ twitter/twitter/func.py
@@ -99,6 +99,11 @@ def auth_user_tokens(dict):
         # response = Response()
         # response.set_cookie('token', new_access_token, httponly=True)
         return dict
+    except jwt.InvalidTokenError:
+        return {'error': 'wrong_token'}
+
+    except jwt.exceptions.DecodeError:
+        return {'error': 'token_not_correct'}
 
 
 # auth_user_tokens("token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjMyODU4MjYyLCJqdGkiOiI3NTNkOTJmMjA1NGU0Y2I4OTVkYjgzZDIzMDQ1NjBhZCIsInVzZXJfaWQiOjI2fQ.19PnXIDLO4pp2lupWnpdSTPiqBdMK3AoqTLdYzYTyLhEFCEZ-ZiUulMawy6nmQA6xjpW-w3WG186AuuKYoFSpQ; refresh=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTYzMjkzNzQ2MiwianRpIjoiODY5OGI4OWQyZDZjNGJkMDk1M2U2N2YwNTMyZTI5ZTMiLCJ1c2VyX2lkIjoyNn0.aqWQah_7606mc0SMMbKHSym6O13WU4rsslcI8BbPHqCYM2QOk8OBHbWjhwQKnQdh6j_zZffpx_gpfMo9My2MvA; username=shahed")
