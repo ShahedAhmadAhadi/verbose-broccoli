8b5343792c7ffeca269bf0d8c965947dce80f868
Shahed Ahmad <shaheda.a16@gmail.com>
2021-10-23 19:42:07 +0430

    user infos update serializer added

--- twitter/user_info/serializers.py
+++ twitter/user_info/serializers.py
@@ -16,3 +16,14 @@ class UserInfoSerializer(serializers.Serializer):
     def create(self, validated_data):
         print(validated_data)
         return UserInfo.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+
+        instance.user = validated_data.get('user', instance.user)
+        instance.DOB = validated_data.get('DOB', instance.DOB)
+        instance.gender = validated_data.get('gender', instance.gender)
+        instance.about = validated_data.get('about', instance.about)
+        instance.image = validated_data.get('image', instance.image)
+        # instance.save()
+
+        return super().update(instance, validated_data)
