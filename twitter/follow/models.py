36aec2aaae3a70d285f48028e5e5aa3edf058867
Shahed Ahmad <shaheda.a16@gmail.com>
2021-10-29 22:09:05 +0430

    new model created for user followers

--- twitter/follow/models.py
+++ twitter/follow/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Followers(models.Model):
+    user_id = models.ForeignKey('User', on_delete=models.CASCADE, related_name="following")
+    following_user_id = models.ForeignKey('User', related_name='followers', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
