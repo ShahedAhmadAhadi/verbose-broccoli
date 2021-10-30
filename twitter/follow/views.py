c1d58212b736d953507b44cf8e18bf53a00c88d1
Shahed Ahmad <shaheda.a16@gmail.com>
2021-10-30 21:14:13 +0430

    new rules and views for follow

--- twitter/follow/views.py
+++ twitter/follow/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 
 # Create your views here.
 
 def follow(request, username):
-    pass
+    data = request.data
+
+    print(username)
+
+    return Response({'result', username})
